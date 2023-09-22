705c426b7dc8c69859d3514370b46b0e2fed6164
DANIEL BRANLEY <dan@macbookpro.mynetworksettings.com>
2023-09-22 11:52:27 -0400

    Styling changes to InlineTextEdit component

--- frontend/src/components/InlineTextEdit.jsx
+++ frontend/src/components/InlineTextEdit.jsx
@@ -6,11 +6,11 @@ import { Avatar,
          Loader, 
          Text,
          TextInput, } from "@mantine/core";
-import { useMediaQuery } from "@mantine/hooks";
+import { useFocusTrap, useMediaQuery } from "@mantine/hooks";
 import PropTypes from "prop-types";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import { Link, useLocation } from "react-router-dom";
-import { IconPencil } from "@tabler/icons-react";
+import { IconCircleCheck, IconCircleX, IconPencil } from "@tabler/icons-react";
 
 
 export default function InlineTextEdit(props) {
@@ -18,23 +18,53 @@ export default function InlineTextEdit(props) {
     console.log("InlineTextEdit - at top - props is:");
     console.log(props);
 
+    const focusTrapRef = useFocusTrap();
+
     const [editing, setEditing] = useState(false);
 
     //Content for the Edit field
 
     let editFieldContent = 
-        <Text size={props.size} fw={props.fontWeight} c={props.color} onClick={()=>setEditing(true)}>{props.text}+InText</Text>;     
+        <Group>
+            <Text size={props.size} 
+                fw={props.fontWeight} 
+                c={props.color} 
+                style={{cursor:"pointer"}}
+                onClick={()=>setEditing(true)}>{props.text}+InText</Text>    
+            <IconPencil strokeWidth={2} 
+                        color={props.color} 
+                        style={{cursor:"pointer"}}
+                        onClick={()=>setEditing(true)}/>
+        </Group>
     
     if (editing){
         editFieldContent = 
-        <TextInput size={props.size} fw={props.fontWeight} c={props.color} value={props.text+"inTextInput"} onBlur={()=>setEditing(false)}/>
+        <Group>
+            <TextInput size={props.size} 
+                    fw={props.fontWeight} 
+                    c={props.color} 
+                    value={props.text+"inTextInput"} 
+                    ref={focusTrapRef}
+                    data-autofocus
+                    onEscap={()=>console.log("Escape pressed!")}
+                    // onBlur={()=>setEditing(false)}
+                    />
+            <IconCircleX color="red" 
+                        //  size={props.size}
+                         style={{cursor:"pointer"}}
+                         onClick={()=>setEditing(false)}/>
+            <IconCircleCheck color="green" 
+                            //  size={props.size}
+                             style={{cursor:"pointer"}}
+                             onClick={()=>setEditing(false)}/>
+            
+        </Group>
     }
         
     return (
-        <Group>
+        <div>
             {editFieldContent}
-            <IconPencil strokeWidth={2} color={props.color}/>
-        </Group>       
+        </div>    
     );
 
 }
