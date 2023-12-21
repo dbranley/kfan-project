9eb64c21da954ab7190ca543876175fedf761589
DANIEL BRANLEY <dan@macbookpro.mynetworksettings.com>
2023-12-21 13:12:20 -0500

    in-progress: trying to add vitest. going to now re-install vite and move stuff into new vite project.

--- frontend-ts/vite.config.ts
+++ frontend-ts/vite.config.ts
@@ -1,9 +1,22 @@
-
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
+
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+    // speed up since tests don't rely on css
+    // https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts#L14-L16
+		css: false,    
+  }
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: vitestConfig.test,
   server: {
     proxy: {
       '/api': 'http://127.0.0.1:8000'
@@ -11,9 +24,17 @@ export default defineConfig({
     host: '127.0.0.1'
   },  
   plugins: [react()],
+  
   // test: {
   //   globals: true,
   //   environment: "jsdom",
   //   setupFiles: "src/setupTests.ts"
-  // }      
-})
+  // }   
+  // test: {
+  //   environment: "jsdom",
+  //   setupFiles: "./setupTests.ts",
+  //   // speed up since tests don't rely on css
+  //   // https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts#L14-L16
+	// 	css: false,
+  // },     
+});
